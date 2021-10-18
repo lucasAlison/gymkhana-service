d4a51bdc8a101205b8aca4517b85fbae47813188
Lucas Alison de Lima <lucasalison.lima926@gmail.com>
2021-10-17 21:36:25 -0300

    FIX - return show user

--- src/modules/users/services/ShowUserService.ts
+++ src/modules/users/services/ShowUserService.ts
@@ -1,9 +1,9 @@
+import ITeamsRepository from '@modules/teams/repositories/ITeamsRepository';
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
 import User from '../infra/typeorm/entities/User';
 
 import IUsersRepository from '../repositories/IUsersRepository';
-
 interface IRequest {
   user_id: string;
 }
@@ -12,7 +12,9 @@ interface IRequest {
 class ShowUserService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject('TeamsRepository')
+    private teamsRepository: ITeamsRepository
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User> {
@@ -21,6 +23,15 @@ class ShowUserService {
     if (!user) {
       throw new AppError('User does not exists');
     }
+
+    if (user.team_id) {
+      const team = await this.teamsRepository.findById(user.team_id);
+
+      if (team){
+        user.team = team;
+      }
+    }
+
 
     return user;
   }
