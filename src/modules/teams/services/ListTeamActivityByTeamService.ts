c3a3c9d349eb4c2247f6c2775d37e1a7729a5ac3
Lucas Alison de Lima <lucasalison.lima926@gmail.com>
2021-11-02 19:01:24 -0300

    FIX - participant in team activity

--- src/modules/teams/services/ListTeamActivityByTeamService.ts
+++ src/modules/teams/services/ListTeamActivityByTeamService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 import TeamActivities from '@modules/teams/infra/typeorm/entities/TeamActivity';
 import ITeamActivitiesRepository from '../repositories/ITeamActivitiesRepository';
 import IActivitiesRepository from '@modules/activities/repositories/IActivitiesRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 interface IRequest {
   team_id: string;
@@ -14,11 +15,11 @@ class ListTeamActivityByTeamService {
     private teamActivitiesRepository: ITeamActivitiesRepository,
     @inject('ActivitiesRepository')
     private activitiesRepository: IActivitiesRepository,
+    @inject('UsersRepository') private usersRepository: IUsersRepository,
   ) {}
 
   public async execute({ team_id }: IRequest): Promise<TeamActivities[]> {
     const teamActivities = await this.teamActivitiesRepository.findAllByTeam( team_id );
-    console.log('inicio');
     for(var item of teamActivities) {
       if (item.activity_id) {
         const activity = await this.activitiesRepository.findById(item.activity_id);
@@ -27,8 +28,14 @@ class ListTeamActivityByTeamService {
           item.activity = activity;
         }
       }
+      if (item.participant_id){
+        const participant = await this.usersRepository.findById(item.participant_id);
+
+        if (participant){
+           item.participant = participant;
+        }
+      }
     }
-    console.log('fim');
     return teamActivities;
   }
 }
